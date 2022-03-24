7b93310549dbccf941921f70c9d7c428fb782f2d
Ankita <anki708@gmail.com>
2022-03-24 14:33:55 +0530

    All files commited

---
+++ src/test/java/com/crm/TestProject/PracticeClass/PracticeLeadTest.java
@@ -0,0 +1,11 @@
+package com.crm.TestProject.PracticeClass;
+
+import org.testng.annotations.Test;
+
+public class PracticeLeadTest {
+
+	@Test
+	public void practiceLead() {
+		System.out.println("lead created in practice project");
+	}
+}
---
+++ src/test/java/com/crm/TestProject/PracticeClass/PracticeOrgTest.java
@@ -0,0 +1,11 @@
+package com.crm.TestProject.PracticeClass;
+
+import org.testng.annotations.Test;
+
+public class PracticeOrgTest {
+
+	@Test
+	public void practiceOrg() {
+		System.out.println("lead created in practice project");
+	}
+}
